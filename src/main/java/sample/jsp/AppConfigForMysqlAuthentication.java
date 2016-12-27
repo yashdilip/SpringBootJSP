5961740aaee5eb949a8fa99d338454eb5624b9f4
Dilip <Dilip@DESKTOP-5Q8N9DO>
2016-12-27 01:47:01 -0800

    Sample Spring Boot project with JSP page for view

---
+++ src/main/java/sample/jsp/AppConfigForMysqlAuthentication.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package sample.jsp;
+
+import javax.servlet.MultipartConfigElement;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * @author Dilip
+ *
+ */
+@EnableWebMvc
+@Configuration
+@ComponentScan("sample.jsp")
+@PropertySource(value = { "classpath:application.properties" })
+public class AppConfigForMysqlAuthentication {
+	@Autowired
+	private Environment environment;
+
+	@Bean(name = "dataSource")
+	public DriverManagerDataSource dataSource() {
+		DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+		driverManagerDataSource.setDriverClassName(environment.getProperty("spring.database.driverClassName"));
+		driverManagerDataSource.setUrl(environment.getProperty("spring.datasource.url"));
+		driverManagerDataSource.setUsername(environment.getProperty("spring.datasource.username"));
+		driverManagerDataSource.setPassword(environment.getProperty("spring.datasource.password"));
+		return driverManagerDataSource;
+	}
+
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/jsp/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+
+	@Bean
+	MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		factory.setMaxFileSize(environment.getProperty("multipart.maxFileSize"));
+		factory.setMaxRequestSize(environment.getProperty("multipart.maxRequestSize"));
+		return factory.createMultipartConfig();
+	}
+
+}
